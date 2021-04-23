f6da99605e274bd52e3dd692bc2c32e4ae63e7fc
Nick <62834545+MikeC0xl0ng@users.noreply.github.com>
2021-04-23 11:00:45 +0200

    Improved user experience and readability of the code
    Orginized the code to functions, improved user experience by adding more understandable messages of I/O, simplified the input function"

--- src/passwordgenerator/PasswordGenerator.java
+++ src/passwordgenerator/PasswordGenerator.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 
 public class PasswordGenerator {
     
+    public static Scanner sc = new Scanner(System.in);
+    
     public static String generatePassword(int length){
+        System.out.println("Generating Password...");
         String password = "";
         int i;
         for(i = 0; i < length; i++)
@@ -19,32 +22,29 @@ public class PasswordGenerator {
         return (char)randomCharASCII;
     }
     
-    public static void main(String[] args) {
-
+    public static void greetingMessage(){
         System.out.println("HEY! I'm a random password generator, do you want to start?");
-        Scanner sc = new Scanner(System.in);
         System.out.print("Press Enter to begin....");
         sc.nextLine();
-        
-        System.out.print("Let me know how long must your password be (between 4 and 20): \r\n"
-                                + "-----> ");
-        
-        int length = -1;
-        while (length > 20 || length < 4) {
-           while(!sc.hasNextInt()){
-               sc.next() ;
-           }
-           length = sc.nextInt();
-           
-        }
-        System.out.println("Generating Password...");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException ex) {}
-        String generatedPassword = generatePassword(length);
-        System.out.println("Your new password is: " + generatedPassword);
-        
-        
     }
     
+    public static  int getLength(){
+        int length = -1;
+        while (length > 30 || length < 4) {
+           System.out.print("-----> ");
+           length = sc.nextInt();    
+        }
+        return length;
+    }
+    
+    public static void main(String[] args) {
+
+        greetingMessage();
+        System.out.println("Password should be at least 4 characters, meanwhile a strong passwords are longer than 8");
+        System.out.print("Let me know how long must your password be: \r\n");
+        int length = getLength();
+        String generatedPassword = generatePassword(length);
+        System.out.println("Your new password is: " + generatedPassword);    
+        
+    } 
 }
